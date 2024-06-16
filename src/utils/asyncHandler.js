82aa74be9574ae6f338e76fb3df0d3b4bb81447b
VarunKumarBantupalli <bantupallivarun@gmail.com>
2024-06-16 16:05:10 +0530

    setting up router and controllers

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,22 +1,22 @@
-// // async handler in  promise method
-// const asyncHandler = (requestHandler) =>{
-//      (req,res,next)=>{
-//         Promise.resolve().catch((err)=>next(err))
-//      }
-// }
+// async handler in  promise method
+const asyncHandler = (requestHandler) =>{
+    return (req,res,next)=>{
+        Promise.resolve().catch((err)=>next(err))
+     }
+}
 
 export {asyncHandler}
 
 
 // async handler in  try catch method
-const asyncHandler = (fn) => async(req,res,next) =>{
-    try{
-        await fn(req,res,next)
+// const asyncHandler = (fn) => async(req,res,next) =>{
+//     try{
+//         await fn(req,res,next)
 
-    }catch(err){
-        res.status(err.code ||500).json({
-            success:false,
-            message:err.message
-        })
-    }
-}
+//     }catch(err){
+//         res.status(err.code ||500).json({
+//             success:false,
+//             message:err.message
+//         })
+//     }
+// }
